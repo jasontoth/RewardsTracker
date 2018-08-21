b08f6f9ae0a00998ad0fd3f1bb2d497dc6777a29
jasontoth <jtoth313@gmail.com>
2018-08-21 17:08:32 -0500

    Initial Commit

---
+++ RewardsTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RewardsTracker
+//
+//  Created by Jason Toth on 8/21/18.
+//  Copyright © 2018 Jason Toth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
