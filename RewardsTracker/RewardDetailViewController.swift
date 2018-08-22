077687b4ff02a50dee313cb29547c7a9e82d3e2d
jasontoth <jtoth313@gmail.com>
2018-08-22 15:19:38 -0500

    - Add placeholder image to reward details view controller - Connect buttons from reward details view controller to RewardDetailViewController class - Rename ViewController to RewardListViewController

---
+++ RewardsTracker/RewardDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RewardDetailViewController.swift
+//  RewardsTracker
+//
+//  Created by Jason Toth on 8/22/18.
+//  Copyright © 2018 Jason Toth. All rights reserved.
+//
+
+import UIKit
+
+class RewardDetailViewController: UIViewController {
+
+    @IBOutlet weak var rewardImageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addExistingPhoto(_ sender: Any) {
+    }
+    
+    @IBAction func addNewPhoto(_ sender: Any) {
+    }
+    
+    @IBAction func saveReward(_ sender: Any) {
+    }
+}
