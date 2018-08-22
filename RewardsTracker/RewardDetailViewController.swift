f21cb8969054fdef55a92e1fe69f2b849dd2c90f
jasontoth <jtoth313@gmail.com>
2018-08-22 16:40:31 -0500

    - Add logic for selecting image from photo library - Implement Core Data for Reward object - Add logic for saving new reward (name and image)

--- RewardsTracker/RewardDetailViewController.swift
+++ RewardsTracker/RewardDetailViewController.swift
@@ -8,23 +8,50 @@
 
 import UIKit
 
-class RewardDetailViewController: UIViewController {
+class RewardDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var rewardImageView: UIImageView!
     @IBOutlet weak var nameTextField: UITextField!
     
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
     
     @IBAction func addExistingPhoto(_ sender: Any) {
+        
+        // Give image picker the ability to source data from saved photos
+        imagePicker.sourceType = .photoLibrary
+        
+        // Present the user with a dialog where they can select existing photos
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // Get the image from the input
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set selected image as the image in rewardImageView
+        rewardImageView.image = image
+        
+        // Dismiss the select photos dialog and go back to reward detail screen
+        imagePicker.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func addNewPhoto(_ sender: Any) {
     }
     
     @IBAction func saveReward(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let reward = Reward(context: context)
+        reward.name = nameTextField.text
+        reward.image = UIImagePNGRepresentation(rewardImageView.image!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
     }
 }
